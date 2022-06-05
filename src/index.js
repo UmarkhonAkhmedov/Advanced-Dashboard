00e8a944b606fbbf3a663f6f2859b38244493cb7
Umarkhon <umarkhonakhmedov2000@gmail.com>
2022-06-05 16:18:33 +0500

    Finishing Sidebar component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import { ContextProvider } from './contexts/ContextProvider';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <ContextProvider> */}
+    <ContextProvider>
       <App />
-    {/* </ContextProvider> */}
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
